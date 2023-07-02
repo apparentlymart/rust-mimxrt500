e248a8e0c6a8f814f64104e43c9434440852ad36
Martin Atkins <mart@degeneration.co.uk>
2023-07-02 12:19:40 -0700

    scratch: Blinky blinky
    The RTT setup here doesn't seem to be working quite right yet, so instead
    we'll use a blinking LED on the eval board as our proof of being able to
    run code on this board.
    
    This intentionally uses very simple direct register writes just to make
    it easier to inspect in a debugger and verify that it's running correctly.

--- scratch/src/main.rs
+++ scratch/src/main.rs
@@ -10,15 +10,36 @@ extern crate mimxrt595s;
 // Must link this to get the flash configuration block.
 extern crate mimxrt595_evk;
 
-use mimxrt500_rt::{entry, pre_init};
+use mimxrt500_rt::entry;
 use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_default};
 
-#[pre_init]
-unsafe fn before_main() {
-    loop {
-        cortex_m::asm::wfi();
-    }
+const _CLKCTL1_BASE_ADDR: u32 = 0x40021000;
+const _RSTCTL1_BASE_ADDR: u32 = 0x40020000;
+const _IOCON_BASE_ADDR: u32 = 0x40004000;
+const _GPIO_BASE_ADDR: u32 = 0x40100000;
+
+const _PRSTCTL1_CLR: u32 = _RSTCTL1_BASE_ADDR + 0x0074;
+
+const _CLKCTL1_PSCCTL1_SET: u32 = _CLKCTL1_BASE_ADDR + 0x0044;
+
+const _GPIO_DIR0: u32 = _GPIO_BASE_ADDR + 0x2000;
+const _GPIO_SET0: u32 = _GPIO_BASE_ADDR + 0x2200;
+const _GPIO_CLR0: u32 = _GPIO_BASE_ADDR + 0x2280;
+const _GPIO_NOT0: u32 = _GPIO_BASE_ADDR + 0x2300;
+
+const _PIO0_14: u32 = _IOCON_BASE_ADDR + 0x0038;
+const _PIO0_26: u32 = _IOCON_BASE_ADDR + 0x0068;
+const _PIO0_31: u32 = _IOCON_BASE_ADDR + 0x007c;
+
+#[inline(always)]
+const fn raw_ptr<T>(addr: u32) -> *mut T {
+    addr as *mut T
+}
+
+#[inline(always)]
+unsafe fn wr32(addr: u32, v: u32) {
+    core::ptr::write_volatile(raw_ptr(addr), v)
 }
 
 #[entry]
@@ -26,7 +47,23 @@ fn main() -> ! {
     rtt_init_default!();
     rprintln!("hello world!");
 
+    const BIT14: u32 = 1 << 14;
+    unsafe {
+        wr32(_CLKCTL1_PSCCTL1_SET, 1); // Enable GPIO port clock
+        wr32(_PRSTCTL1_CLR, 1); // Take GPIO out of reset
+        wr32(_PIO0_14, 0); // Disable alternate function
+        wr32(_GPIO_CLR0, BIT14); // LED starts off
+        wr32(_GPIO_DIR0, BIT14); // Our pin is an output
+    }
+
     loop {
-        cortex_m::asm::wfi();
+        // Toggle the LED, using the "NOT" register
+        unsafe {
+            wr32(_GPIO_NOT0, BIT14);
+        }
+
+        for _ in 0..80000 {
+            cortex_m::asm::nop();
+        }
     }
 }
